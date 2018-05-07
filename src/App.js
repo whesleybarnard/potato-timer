403c0cbb67f747759d8e79f7c99fbe65aaac025d
Whesley Barnard <whesley.barnard@gmail.com>
2018-05-07 15:49:30 +0200

    played around with some sass colours and susy grids

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ class App extends Component {
   render() {
     return (
       <div>
-        <header></header>
+        <header>{test2('woohoo')}</header>
         <div className="grid">
           <div className="grid-item"></div>
+          <div className="grid-item inner-grid">
+            <div className="inner-grid-item"></div>
+            <div className="inner-grid-item"></div>
+            <div className="inner-grid-item"></div>
+          </div>
           <div className="grid-item"></div>
-          <div className="grid-item"></div>
-          <div className="grid-item"></div>
-          <div className="grid-item"></div>
+        </div>
+        <div className="grid2">
+          <div className="grid2-item"></div>
+          <div className="grid2-item"></div>
+          <div className="grid2-item"></div>
+          <div className="grid2-item"></div>
         </div>
       </div>
     );
   }
+}
+
+const test2 = (tt) => {
+  return tt;
 }
 
 export default App;
