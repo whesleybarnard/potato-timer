af924ec898491e4d3b777d8abfcf511f4593b31e
Whesley Barnard <whesley.barnard@gmail.com>
2018-05-11 15:27:12 +0200

    changed some html. still messing around with the design. Trying different things.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header>{test2('woohoo')}</header>
-        <div className="grid">
-          <div className="grid-item"></div>
-          <div className="grid-item inner-grid">
-            <div className="inner-grid-item"></div>
-            <div className="inner-grid-item"></div>
-            <div className="inner-grid-item"></div>
-          </div>
-          <div className="grid-item"></div>
-        </div>
-        <div className="grid2">
-          <div className="grid2-item"></div>
-          <div className="grid2-item"></div>
-          <div className="grid2-item"></div>
-          <div className="grid2-item"></div>
+        <header className="app-header">a heading</header>
+        <div className="app-body">
+          <main className="app-main">app-main</main>
+          <aside className="app-aside">app-aside</aside>
         </div>
       </div>
     );
   }
-}
-
-const test2 = (tt) => {
-  return tt;
 }
 
 export default App;
